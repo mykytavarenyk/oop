4d5d2852c8659e09fd195685aa95339c30ab3dea
Mykyta <mykytavar@gmail.com>
2020-12-31 13:57:58 +0200

    First homework added

---
+++ homework_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace homework_1
+{
+    class Program
+    {     
+       public static void Main(string[] args)
+       {
+          Console.Write("Enter x:");
+
+          double x = Convert.ToDouble(Console.ReadLine());
+
+          double result = Math.Tan(x - Math.PI);
+
+          Console.WriteLine(result);
+       }
+    }
+}
