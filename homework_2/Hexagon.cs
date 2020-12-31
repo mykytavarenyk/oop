3f76746a190f70aa734517d99c6d57e0f6dfffa7
Mykyta <mykytavar@gmail.com>
2020-12-31 14:36:10 +0200

    Second homework added

---
+++ homework_2/Hexagon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace homework_2
+{
+    class Hexagon
+    {
+        private double sideLength;
+
+        public double GetSideLength()
+        {
+            return sideLength;
+        }
+
+        public void SetSideLength(double sideLength)
+        {
+            this.sideLength = sideLength;
+        }
+
+        public double CalculateArea()
+        {
+            return (Math.Pow(sideLength, 2) * 3 * Math.Sqrt(3) / 2);
+        }
+
+        public Hexagon(double sideLength)
+        {
+            this.sideLength = sideLength;
+        }
+
+        ~Hexagon()
+        {
+            Console.WriteLine("Hexagon destroyed");
+        }
+    }
+}
---
+++ homework_2/Runner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homework_2
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            Hexagon hexagon = new Hexagon(3);
+
+            Console.WriteLine("Площа правильного шестикутнику :" + hexagon.CalculateArea());
+
+            hexagon.SetSideLength(7);
+
+            Console.WriteLine("Площа правильного шестикутнику :" + hexagon.CalculateArea());
+
+            Console.WriteLine("Сторона правильного шестикутнику :" + hexagon.GetSideLength());
+        }
+    }
+}
